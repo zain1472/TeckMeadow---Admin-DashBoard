a98961526317eefd56c0a2e7fb83dffe21106fc0
Zain Ul Abideen <zain.abideen14572@gmail.com>
2020-04-12 21:56:05 +0500

    ...

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ io.on("connection", function (socket) {
 });
 
 const root = require("path").join(__dirname, "client", "build");
-
+app.use(express.static(root));
 app.get("*", (req, res) => {
   res.sendFile("index.html", { root });
 });
