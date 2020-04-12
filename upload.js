f900a0504412598d4abc72973cfcf0906f1c7800
Zain Ul Abideen <zain.abideen14572@gmail.com>
2020-04-12 21:07:34 +0500

    ...

--- upload.js
+++ upload.js
@@ -1,7 +1,7 @@
 var multer = require("multer");
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/uploads");
+    cb(null, __dirname + "/public/uploads");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
